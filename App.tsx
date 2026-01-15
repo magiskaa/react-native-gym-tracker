26166298123d3a13aae3d7ccbcd862ca40c9f7a0
Valtteri Antikainen <vantikaine@gmail.com>
2026-01-15 14:32:02 +0200

    Created a tab layout

---
+++ App.tsx
@@ -0,0 +1,52 @@
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import HomeScreen from "./src/screens/HomeScreen";
+import WorkoutScreen from "./src/screens/WorkoutScreen";
+import PhaseScreen from "./src/screens/PhaseScreen";
+import StatsScreen from "./src/screens/StatsScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+	return (
+		<NavigationContainer>
+			<StatusBar style="auto" />
+			<Tab.Navigator
+				screenOptions={({ route }) => ({
+					tabBarIcon: ({ color, size }) => {
+						let iconName: keyof typeof Ionicons.glyphMap;
+
+						switch (route.name) {
+							case "Home":
+								iconName = "home";
+								break;
+							case "Workout":
+								iconName = "barbell";
+								break;
+							case "Phase":
+								iconName = "fitness";
+								break;
+							case "Stats":
+								iconName = "stats-chart";
+								break;
+							case "Profile":
+							default:
+								iconName = "person";
+						}
+
+						return <Ionicons name={iconName} size={size} color={color} />;
+					},
+				})}
+			>
+				<Tab.Screen name="Home" component={HomeScreen} />
+				<Tab.Screen name="Workout" component={WorkoutScreen} />
+				<Tab.Screen name="Phase" component={PhaseScreen} />
+				<Tab.Screen name="Stats" component={StatsScreen} />
+				<Tab.Screen name="Profile" component={ProfileScreen} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
+}
